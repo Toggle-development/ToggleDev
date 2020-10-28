d2af165ef5e2663574cc990ebd0935fde7c99180
Andrew Hernandez <toggledevelopment@gmail.com>
2020-10-27 21:59:18 -0700

    Initial Commit

---
+++ Toggle/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Toggle
+//
+//  Created by user185695 on 10/27/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Toggle/ToggleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToggleApp.swift
+//  Toggle
+//
+//  Created by user185695 on 10/27/20.
+//
+
+import SwiftUI
+
+@main
+struct ToggleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
