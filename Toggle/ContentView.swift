6b15e2f984191c86087f3640d914e9243488549c
JamesMcDougallJr <jmcdouga@ucsd.edu>
2020-11-19 19:15:20 -0800

    Minor updates

--- Toggle/ContentView.swift
+++ Toggle/ContentView.swift
@@ -7,10 +7,23 @@
 
 import Foundation
 import SwiftUI
+import Amplify
+
+func signIn(username: String, password: String) {
+    Amplify.Auth.signIn(username: username, password: password) { result in
+        switch result {
+        case .success:
+            print("Sign in succeeded")
+        case .failure(let error):
+            print("Sign in failed \(error)")
+        }
+    }
+}
 
 struct ContentView: View {
     
     func performOnAppear() {
+        signIn("james", "12345")
         let dataHandle = Database()
         dataHandle.queryPosts()
         let post1 = Post(title: "Awesome Headshot",
